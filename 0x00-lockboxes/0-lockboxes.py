6065cca4c36a1fa511c385152fd3e8fb3d0de8f7
Santiago <sancastillog@gmail.com>
2021-01-29 11:40:52 -0500

    lockboxes with opened array

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,15 +10,14 @@ def canUnlockAll(boxes):
     Method that checks if every box can be opened
     """
     opened = [0]
-
     if len(boxes) == 0:
-        return False
-    for boxIndex in opened:
-        for key in boxes[boxIndex]:
+        return True
+    for box in opened:
+        for key in boxes[box]:
             if key not in opened:
                 if key < len(boxes):
                     opened.append(key)
-        print(opened)
+
     if len(opened) == len(boxes):
         return True
     return False
